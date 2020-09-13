bbe3d29a7cb7ef19697b33288a3b779fb01f0d2f
RiquelvynM <rikelvinsuazo@gmail.com>
2020-09-13 12:38:39 -0400

    initial commit

---
+++ ProyectoSC/ProyectoSC/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ProyectoSC.Models;
+using ProyectoSC.ViewModels;
+
+namespace ProyectoSC.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
